44921e230fb48faf066df6f03ee2f9a402d25c51
jalen201 <jalenjack201@hotmail.com>
2017-12-27 22:45:19 -0500

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,41 @@
+var socket;
+var text = {
+    text: ''
+};
+
+function setup(){
+    socket = io.connect('http://localhost:8080');
+    $("#text").on("froalaEditor.keyup", function(){
+        var html = $(this).froalaEditor('html.get');
+        var data = {
+            text: html
+        };
+        socket.emit('text', data);
+    });
+    $('#text').froalaEditor({
+        toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo'],
+        fullPage: true
+    });
+
+    socket.on('text', handleRecievedText);
+    socket.on('newUser', updateText);
+}
+
+function updateText(data){
+    text.text = data.text;
+    $("#text").froalaEditor('html.set', data.text);
+    var editor = $('#text').data('froala.editor');
+    editor.selection.setAtEnd(editor.$el.get(0));
+    editor.selection.restore();
+}
+
+function handleRecievedText(data){
+    console.log(data);
+    text.text = data.text;
+    $("#text").froalaEditor('html.set', data.text);
+    var editor = $('#text').data('froala.editor');
+    editor.selection.setAtEnd(editor.$el.get(0));
+    editor.selection.restore();
+}
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+var server = app.listen(8080);
+app.use(express.static('public'));
+console.log('Server listening on port 8080');
+var io = socket(server);
+
+io.sockets.on('connection', connection);
+
+var text = {
+    text: ''
+};
+
+function connection(socket){
+    console.log('a new user with id ' + socket.id + " has entered");
+    socket.emit('newUser', text);
+
+    socket.on('text', handleTextSent);
+
+    function handleTextSent(data){
+        text.text = data.text
+        io.sockets.emit('text', data);
+    }
+}
+
